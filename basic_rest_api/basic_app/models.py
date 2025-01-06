fd758703f723231303efd88f281c5d30e140303f
ANUSHA <anushaganeshan06@gmail.com>
2025-01-06 19:57:47 +0530

    commit

--- basic_rest_api/basic_app/models.py
+++ basic_rest_api/basic_app/models.py
@@ -4,5 +4,6 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
 
+    
     def __str__(self):
         return self.title
--- basic_rest_api/basic_app/views.py
+++ basic_rest_api/basic_app/views.py
@@ -10,7 +10,7 @@ def task_list(request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
-
+    
     elif request.method == 'POST':
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
